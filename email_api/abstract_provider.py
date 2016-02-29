b1a42e2005019e25cec76acd29ecfd67bfc3c9d4
Julien Marechal <julien.marechal35@gmail.com>
2016-02-29 17:18:51 +0100

    Cleanup doc

--- email_api/abstract_provider.py
+++ email_api/abstract_provider.py
@@ -1,11 +1,12 @@
 """This module contains everything related to providers.
-Third party provider API implementation/definition must use the below.
 
-The role of AProvider subclasses is just to take in core data struct,
-shape and massage it to fit the specific API formats.
+Third party provider's API implementation/definition must use the below.
 
-It should not do any IOs or fancy side effects, the Manager takes care
-of that.
+The role of an AProvider subclass is just to take in core data struct,
+shape and massage it to fit its specific API format.
+
+It should not do any IOs or fancy side effects, the
+`email_api.providers_manager.Manager` takes care of that.
 
 """
 
@@ -14,7 +15,7 @@ from abc import ABC, abstractproperty, abstractmethod
 
 
 class InvalidProviderError(Exception):
-    """Raise if you detect an invalid provider at runtime
+    """Raise if you detect an invalid provider at runtime.
 
     Could happen if it's untested, because we have no way to enforce
     the return types with ABC or others (AFAIK)
@@ -24,12 +25,11 @@ class InvalidProviderError(Exception):
 
 
 class DataFormat(Enum):
-    """ Enum values map to
-    requests.request() arguments for sending data
+    """ Enum values map to `requests.request()` arguments for sending data.
 
-    Use form for POST form parameters
-    Use query for URL query parameters
-    User JSON for... json
+    Use `form` for POST form parameters
+    Use `query` for URL query parameters
+    User `json` for... json
     """
     form = 'data'
     json = 'json'
@@ -37,19 +37,19 @@ class DataFormat(Enum):
 
 
 class HttpMethod(Enum):
-    """ Enum values map to HTTP Methods
+    """ Enum values map to HTTP Methods.
     """
     post = 'POST'
     get = 'GET'
 
 
 class AProvider(ABC):
-    """Third party provider must implement this abstract class
+    """Third party providers must implement this abstract class
 
     Proper return types for subclasses are (to be) validated in the unit test
 
     `self._user` and `self._key` should be used in the subclass for auth.
-    `config` is saved under self._config if need be
+    `config` is saved under `self._config` if need be
     """
 
     nickname = None
@@ -59,6 +59,11 @@ class AProvider(ABC):
     Subclasses should have their own
     """
     def __init__(self, config=None):
+        """
+        Args:
+            config (Optionnal[dict]): Application config, should contain
+              api username and key/password
+        """
         self._user = ''
         self._key = ''
         self._config = config
@@ -72,63 +77,67 @@ class AProvider(ABC):
         self._user = conf.get('user', self._user)
         self._key = conf.get('key', self._key)
 
-
     @abstractproperty
     def auth(self):
-        """
-        Return None if the API does not use/need HTTP auth
+        """Return None if the API does not use/need HTTP auth.
 
         Returns:
-          tuple: containing (username:str, password:str) for http auth
+            tuple: containing (username:str, password:str) for http auth
         """
         pass
 
     @abstractproperty
     def send_url(self):
-        """
-        Return the HTTP method and the API url to use for sending
-        emails
+        """Returns the HTTP method and the API url to use for sending
+        emails.
 
         Returns:
-          tuple: containing (provider.HttpMethod, http_url:str)
+            A tuple containing, the http method to use and the url::
+
+                (provider.HttpMethod, str)
 
         """
         pass
 
     @abstractmethod
     def email_to_data(self, email):
-        """This method takes an `email_api.message.Email` class and turns it
-        into a dict, adding API specific extra parameters, changing
+        """Format core data to API specific data format.
+
+        This method takes an `email_api.message.Email` class and turns it
+        into a (dict): adding API specific extra parameters, changing the
         structure, key names, etc... If need be.
 
-        The provider sub class must also be aware of
-        `email_api.message.Recipient` and adapt it to its need.
+        The provider sub class must also be aware of the email recipient
+        data structure: `email_api.message.Recipient` and adapt it to its need.
 
-        Files are currently not implemented in email_api.message but
-        they should also be wrapped in a class such as Attachment
-
-        Returns of tuple with the `email_api.provider.DataFormat` to
-        use and the email as a dict
+        Files are currently not implemented in `email_api.message` but
+        they should also be wrapped in a class such as `Attachment`
 
         Args:
-          email (email_api.message.Email): The email to be serialized
-            to API specific format
+            email (email_api.message.Email): The email to be serialized
+              to API specific format
 
         Returns:
-          tuple: (email_api.provider.DataFormat, dict)
+            A (tuple) containing the `email_api.provider.DataFormat` to
+            use and the email as a (dict)::
+
+                (email_api.provider.DataFormat, dict)
 
         """
         pass
 
     def validate(self):
-        """This method just makes sure the Subclass is implemented
+        """Validates subclass implementation.
+
+        This method just makes sure the Subclass is implemented
         properly, not to have random ValueError when unpacking values.
 
-        Note: You must call it explicitly. We don't check that the url
-        is valid, we delegate to the lib  sending the requests
+        Note:
+            You must call it explicitly. We don't check that the url
+            is valid, we delegate to the lib sending the requests.
 
         Raises:
-          InvalidProviderError
+            InvalidProviderError
 
         """
         if self.auth is not None:
